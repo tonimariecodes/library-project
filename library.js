f34e13e11ba14f2847a2eb68c35e754db1877eee
tonimariecodes <tonimariecodes@gmail.com>
2026-01-04 21:21:33 -0700

    created library in js file

---
+++ library.js
@@ -0,0 +1,16 @@
+console.clear();
+
+const myLibrary = [];
+
+function Book() {
+  this.id = crypto.randomUUID();
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.isRead = isRead;
+}
+
+function addBookToLibrary(title, author, pages, isRead) {
+  const newBook = new Book(title, author, pages, isRead);
+  myLibrary.push(newBook);
+}
